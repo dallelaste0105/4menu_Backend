761a3e4add0dc22ef6bc217edff8a5305515f5ae
dallalaste <gabrieldallelaste75@gmail.com>
2025-08-30 16:34:59 -0300

    feat: login

---
+++ src/models/userModel.js
@@ -0,0 +1,8 @@
+const db = require('../db')
+
+function createUser(name, email, password, callback){
+    const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+    db.query(query, [name,email,password], callback)
+}
+
+module.exports = {createUser}
